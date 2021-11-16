ac712f34e13ee2cfded16a7db9b78cca7e10569b
Karel Pacheco Ramírez <54755265+skykarel@users.noreply.github.com>
2021-11-15 19:16:12 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import scanner
+import ports #ports_and_services
 
-ports = scanner.getOpenPorts("www.google.com", range(1000))
-print("Open ports:", ports)
+ports = scanner.getOpenPorts("www.google.com", range(24,26))
+print("\nPuertos/Servicios")
+for i in ports:
+    print(" " + str(i) + " " + ports_and_services[i])
+#print("Open ports:", ports)
